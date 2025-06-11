524b6709e16fbc04fcf311b719c2582fc8e72980
Olof Johansson <olof@liberdev.se>
2025-06-11 08:13:43 +0200

    work in progress

--- classes/class-menu-manager.php
+++ classes/class-menu-manager.php
@@ -51,20 +51,20 @@ class Menu_Manager {
 
 	public function render_votation_results() {
 		if ( ! FVS_VOTATION_FORM_IDS ) {
-			require_once __DIR__ . '/../templates/votation_results.php';
+			require_once __DIR__ . '/../templates/results.php';
 			return;
 		}
 		$votation_results_db     = $this->results_fetcher->get_votation_results();
 		$votes_per_ip_results_db = $this->results_fetcher->get_votes_per_ip_results();
-		require_once __DIR__ . '/../templates/votation_results.php';
+		require_once __DIR__ . '/../templates/results.php';
 	}
 
 	public function render_votation_manual() {
-		require_once __DIR__ . '/../templates/votation_manual.php';
+		require_once __DIR__ . '/../templates/manual.php';
 	}
 
 	public function render_votation_settings() {
 		$fvs_votation_forminator_forms = Forminator_API::get_forms();
-		require_once __DIR__ . '/../templates/votation_settings.php';
+		require_once __DIR__ . '/../templates/settings.php';
 	}
 }
