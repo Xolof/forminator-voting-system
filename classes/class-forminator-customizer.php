af23e751ad9985dd2479956d4a7de89d6a1249d3
Olof Johansson <olof@liberdev.se>
2025-06-11 14:50:28 +0200

    fix code styel

--- classes/class-forminator-customizer.php
+++ classes/class-forminator-customizer.php
@@ -1,6 +1,7 @@
 <?php
 /**
  * Forminator_Customizer
+ *
  * @package Forminator Voting System
  */
 
@@ -17,14 +18,26 @@ class Forminator_Customizer {
 
 	protected Results_Fetcher $results_fetcher;
 
-	const IP_BLOCKED_MESSAGE         = 'Din IP-adress har blockerats.';
-	const ONLY_VOTE_ONE_TIME_MESSAGE = 'Du har redan röstat på det här alternativet med den här epostadressen.';
-	const IP_ALREADY_VOTED_MESSAGE   = 'Någon har redan röstat på det här alternativet med den här IP-adressen.';
+	const IP_BLOCKED_MESSAGE         = 'Your IP address has been blocked.';
+	const ONLY_VOTE_ONE_TIME_MESSAGE = 'You have already voted for this alternative with this email address.';
+	const IP_ALREADY_VOTED_MESSAGE   = 'Someone has already voted for this alternative with this IP address.';
 
+	/**
+	 * Constructor
+	 *
+	 * @param Results_Fetcher $results_fetcher
+	 */
 	public function __construct( Results_Fetcher $results_fetcher ) {
 		$this->results_fetcher = $results_fetcher;
 	}
 
+	/**
+	 * Add a custom error message to the form.
+	 *
+	 * @param array   $response
+	 * @param integer $form_id
+	 * @return array
+	 */
 	public function custom_error_message( array $response, int $form_id ): array {
 		if ( in_array( intval( $form_id ), FVS_VOTATION_FORM_IDS, true ) ) {
 			if ( ! $response['success'] && isset( $response['message'] ) ) {
@@ -51,17 +64,32 @@ class Forminator_Customizer {
 		return $response;
 	}
 
+	/**
+	 * If the users IP is blocked, add an error to the form.
+	 *
+	 * @param array   $submit_errors
+	 * @param integer $form_id
+	 * @return array $submit_errors
+	 */
 	public function submit_errors_ip_blocked( array $submit_errors, int $form_id ): array {
 		if ( in_array( intval( $form_id ), FVS_VOTATION_FORM_IDS, true ) ) {
 			$user_ip = Forminator_Geo::get_user_ip();
 			if ( in_array( $user_ip, FVS_IP_BLOCK_LIST, true ) ) {
 				// Put each error in an array due to how Forminator prints errors in a hidden list.
-				$submit_errors[]['fvs-ip-blocked'] = self::IP_BLOCKED_MESSAGE;
+				$submit_errors[]['fvs-ip-blocked'] = esc_html__( self::IP_BLOCKED_MESSAGE, 'fvs' );
 			}
 		}
 		return $submit_errors;
 	}
 
+	/**
+	 * Check if email is missing or has already voted.
+	 *
+	 * @param array   $submit_errors
+	 * @param integer $form_id
+	 * @param array   $field_data
+	 * @return array $submit_errors
+	 */
 	public function submit_errors_email( array $submit_errors, int $form_id, array $field_data ): array {
 		if ( in_array( intval( $form_id ), FVS_VOTATION_FORM_IDS, true ) ) {
 			if ( 0 === count( $field_data ) ) {
@@ -72,12 +100,19 @@ class Forminator_Customizer {
 			$email = $field_data[0]['value'];
 			if ( $this->email_has_already_voted( $email, $form_id ) ) {
 				// Put each error in an array due to how Forminator prints errors in a hidden list.
-				$submit_errors[]['fvs-email-already-voted'] = self::ONLY_VOTE_ONE_TIME_MESSAGE;
+				$submit_errors[]['fvs-email-already-voted'] = esc_html__( self::ONLY_VOTE_ONE_TIME_MESSAGE, 'fvs' );
 			}
 		}
 		return $submit_errors;
 	}
 
+	/**
+	 * Check if the IP address has alreadyh voted.
+	 *
+	 * @param array   $submit_errors
+	 * @param integer $form_id
+	 * @return array submit_errors
+	 */
 	public function submit_errors_ip_already_voted( array $submit_errors, int $form_id ): array {
 		if ( FVS_ALLOW_MULTIPLE_VOTES_FROM_SAME_IP === 'yes' ) {
 			return $submit_errors;
@@ -89,11 +124,18 @@ class Forminator_Customizer {
 
 		if ( $this->ip_already_voted( $form_id ) ) {
 			// Put each error in an array due to how Forminator prints errors in a hidden list.
-			$submit_errors[]['fvs-ip-already-voted'] = self::IP_ALREADY_VOTED_MESSAGE;
+			$submit_errors[]['fvs-ip-already-voted'] = esc_html__( self::IP_ALREADY_VOTED_MESSAGE, 'fvs' );
 		}
 		return $submit_errors;
 	}
 
+	/**
+	 * Check if an email has already voted.
+	 *
+	 * @param string  $email
+	 * @param integer $form_id
+	 * @return boolean
+	 */
 	protected function email_has_already_voted( string $email, int $form_id ): bool {
 		global $wpdb;
 		$frmt_form_entry      = $this->results_fetcher->get_table_name_with_prefix( 'frmt_form_entry' );
@@ -124,6 +166,12 @@ class Forminator_Customizer {
 		return false;
 	}
 
+	/**
+	 * Check if an IP address has already voted.
+	 *
+	 * @param integer $form_id
+	 * @return boolean
+	 */
 	protected function ip_already_voted( int $form_id ): bool {
 		$user_ip = Forminator_Geo::get_user_ip();
 		if ( ! empty( $user_ip ) ) {
--- classes/class-settings-processor.php
+++ classes/class-settings-processor.php
@@ -3,6 +3,8 @@
  * Settings_Processor
  *
  * Processes settings for the votation.
+ *
+ * @package Forminator Votation System
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -18,6 +20,11 @@ class Settings_Processor {
 
 	protected const SETTINGS_PAGE_URL = 'admin.php?page=render_votation_settings';
 
+	/**
+	 * Process settings.
+	 *
+	 * @return void
+	 */
 	public function process_settings(): void {
 		$invalid_message = esc_html__( 'Invalid nonce specified.', 'fvs' );
 
@@ -43,17 +50,29 @@ class Settings_Processor {
 		$this->fvs_wp_die( $invalid_message, self::SETTINGS_PAGE_URL );
 	}
 
+	/**
+	 * Custom wp_die. Displaying an error message with a back link.
+	 *
+	 * @param string $message
+	 * @param string $back_link
+	 * @return void
+	 */
 	protected function fvs_wp_die( string $message, string $back_link ): void {
 		wp_die(
 			esc_html( $message ),
 			esc_html__( 'Error', 'fvs' ),
 			array(
-				'response'  => 403,
+				'response'  => 500,
 				'back_link' => esc_html( $back_link ),
 			)
 		);
 	}
 
+	/**
+	 * Process blocked IP addresses.
+	 *
+	 * @return mixed
+	 */
 	protected function process_blocked_ips(): mixed {
 		if ( ! isset( $_POST['blocked_ips'] ) ) {
 			$blocked_ips = array();
@@ -85,6 +104,11 @@ class Settings_Processor {
 		return $this->process_option( 'fvs_votation_blocked_ips', $blocked_ips );
 	}
 
+	/**
+	 * Process ids of Forminator forms.
+	 *
+	 * @return mixed
+	 */
 	protected function process_form_ids(): mixed {
 		if ( isset( $_POST['form_ids'] ) ) {
 			$votation_forminator_form_ids = array_keys( $_POST['form_ids'] );
@@ -101,6 +125,11 @@ class Settings_Processor {
 		return $this->process_option( 'fvs_votation_forminator_form_ids', $votation_forminator_form_ids );
 	}
 
+	/**
+	 * Process the option for allowing several votes from the same IP address.
+	 *
+	 * @return mixed
+	 */
 	protected function process_multiple_votes_from_same_ip(): mixed {
 		if ( isset( $_POST['fvs_allow_multiple_votes_from_same_ip'] ) ) {
 			$fvs_allow_multiple_votes_from_same_ip = sanitize_text_field( wp_unslash( $_POST['fvs_allow_multiple_votes_from_same_ip'] ) );
@@ -112,6 +141,13 @@ class Settings_Processor {
 		}
 	}
 
+	/**
+	 * Save the option if it has a new value, create it if it does not exist.
+	 *
+	 * @param string $option_name
+	 * @param array|string $post_data
+	 * @return boolean
+	 */
 	protected function process_option( string $option_name, array|string $post_data ): bool {
 		$result = true;
 		if ( ! get_option( $option_name ) ) {
@@ -123,6 +159,13 @@ class Settings_Processor {
 		return $result;
 	}
 
+	/**
+	 * Set a flash message.
+	 *
+	 * @param string $type
+	 * @param string $message
+	 * @return void
+	 */
 	protected function set_flash_message( string $type, string $message ): void {
 		set_transient(
 			'fvs_flash_message',
@@ -134,6 +177,11 @@ class Settings_Processor {
 		);
 	}
 
+	/**
+	 * Redirect to the settings page.
+	 *
+	 * @return void
+	 */
 	protected function redirect(): void {
 		wp_safe_redirect(
 			esc_url_raw(
