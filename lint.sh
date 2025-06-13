a6269a91f1e1fd7c30770ae4b4d515784ef9c093
Olof Johansson <olof@liberdev.se>
2025-06-13 20:56:27 +0200

    update linte script

--- lint.sh
+++ lint.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 
-vendor/bin/phpcbf -ps classes/ debug/ templates/ forminator-voting-system.php 
+vendor/bin/phpcbf -ps src/ debug/ templates/ forminator-voting-system.php 
 
-vendor/bin/phpcs -ps classes/ debug/ templates/ forminator-voting-system.php 
+vendor/bin/phpcs -ps src/ debug/ templates/ forminator-voting-system.php 
