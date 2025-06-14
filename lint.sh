ca8c6ff0b90f4aab54b3f62ad5c0f3343ff3f936
Olof Johansson <olof@liberdev.se>
2025-06-14 07:33:53 +0200

    Add config for phpcbf and phpstan, as well as a script for running them

---
+++ lint.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+
+vendor/bin/phpcbf -ps src/ debug/ templates/ forminator-voting-system.php 
+
+vendor/bin/phpcs -ps src/ debug/ templates/ forminator-voting-system.php 
+
+vendor/bin/phpstan analyse
