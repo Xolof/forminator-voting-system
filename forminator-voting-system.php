079d969b16a1501d73ce1c8a852d1a8867f0e80e
Olof Johansson <olof@liberdev.se>
2025-06-14 07:13:40 +0200

    set up the main structure of the app in the main plugin file.
    there is a main class VotingSystem into which other classes are injected. we start up everything with the init method of the class VotingSystem.

--- forminator-voting-system.php
+++ forminator-voting-system.php
@@ -15,3 +15,31 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
+
+require_once __DIR__ . '/autoload.php';
+require_once __DIR__ . '/debug/functions.php';
+
+use ForminatorVotingSystem\VotingSystem;
+use ForminatorVotingSystem\ForminatorCustomizer;
+use ForminatorVotingSystem\SettingsProcessor;
+use ForminatorVotingSystem\ResultsFetcher;
+use ForminatorVotingSystem\MenuManager;
+use ForminatorVotingSystem\FvsLogger;
+use ForminatorVotingSystem\Wrapper\ForminatorFormEntryModelWrapper;
+use ForminatorVotingSystem\Wrapper\ForminatorGeoWrapper;
+
+$forminator_geo_wrapper              = new ForminatorGeoWrapper();
+$forminator_form_entry_model_wrapper = new ForminatorFormEntryModelWrapper();
+
+$results_fetcher       = new ResultsFetcher();
+$settings_processor    = new SettingsProcessor();
+$menu_manager          = new MenuManager( $results_fetcher );
+$forminator_customizer = new ForminatorCustomizer( $results_fetcher, $forminator_geo_wrapper, $forminator_form_entry_model_wrapper );
+
+$voting_system = new VotingSystem(
+	$settings_processor,
+	$menu_manager,
+	$forminator_customizer
+);
+
+$voting_system->init();
