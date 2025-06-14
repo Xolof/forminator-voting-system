d29ab15749f48454db18990ef24e185191801798
Olof Johansson <olof@liberdev.se>
2025-06-14 07:06:48 +0200

    first commit

---
+++ forminator-voting-system.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Plugin Name:     Forminator Voting System
+ * Plugin URI:      https://github.com/xolof/forminator-voting-system
+ * Description:     A voting system using Forminator forms
+ * Author:          Olof Johansson
+ * Author URI:      https://oljo.online
+ * Text Domain:     fvs
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Forminator_voting_system
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
